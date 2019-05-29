1f52b6a74429f8af6ed2007165a5d422e01efec8
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-05-29 01:51:02 +0000

    Mumuki Export on 2019-05-29 01:51:02 +0000

--- 00006_Entre N e M/test.php
+++ 00006_Entre N e M/test.php
@@ -1,19 +1,22 @@
 public function testDescriptionExample(): void {
-  $this->assertTrue(function_exists("entreDosNumeros"), "No existe la función entreDosNumeros");
+  $this->assertTrue(function_exists("entreDoisNumeros"), "A função entreDoisNumeros não existe");
   
-  $fct = new ReflectionFunction('entreDosNumeros');
+  $fct = new ReflectionFunction('entreDoisNumeros');
   $fct = $fct->getNumberOfRequiredParameters();
   
-  $this->assertTrue($fct == 3, "La función debe recibir 3 parámetros");
+  $this->assertTrue($fct == 3, "A função deve receber três parâmetros");
   
-  $this->assertTrue(entreDosNumeros(50, 10, 60) == true, "La función falla para el número 50 con 10 y 60 cómo límites");
-  $this->assertTrue(entreDosNumeros(0, 0, 10) == true, "La función falla para el número 0 con 0 y 10 como límites");
-  $this->assertTrue(entreDosNumeros(100, 90, 100) == true, "La función falla para el número 100 con 90 y 100 cómo límites");
+  $this->assertTrue(entreDosNumeros(50, 10, 60) == true, "Sua função não está funcionando com o número 50 e os números 10 e 60 como limite");
   
-  $this->assertTrue(entreDosNumeros(200, 100, 150) == false, "La función falla para el número 200 con 100 y 150 como límites");
-  $this->assertTrue(entreDosNumeros(101, 80, 100) == false, "La función falla para el número 101 con 80 y 100 cómo límites");
+  $this->assertTrue(entreDosNumeros(0, 0, 10) == true, "Sua função não está funcionando com o número 0 e os números 0 e 10 como limite");
   
+  $this->assertTrue(entreDosNumeros(100, 90, 100) == true, "Sua função não está funcionando com o número 100 e os números 90 e 100 como limite");
   
-  $this->assertTrue(entreDosNumeros(-90, -50, 0) == false, "La función falla para el número -90 con -50 y 0 cómo límites");
-  $this->assertTrue(entreDosNumeros(-1, 0, 10) == false, "La función falla para el número -1 con 0 y 10 cómo límites");
+  $this->assertTrue(entreDosNumeros(200, 100, 150) == false, "Sua função não está funcionando com o número 200 e os números 10 e 150 como limite");
+  
+  $this->assertTrue(entreDosNumeros(101, 80, 100) == false, "Sua função não está funcionando com o número 101 e os números 80 e 100 como limite");
+  
+  $this->assertTrue(entreDosNumeros(-90, -50, 0) == false, "Sua função não está funcionando com o número -90 e os números -50 e 0 como limite");
+  
+  $this->assertTrue(entreDosNumeros(-1, 0, 10) == false, "Sua função não está funcionando com o número -1 e os números 0 e 10 como limite");
 }
