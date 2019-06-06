dc19379229ed605b380943eea1adf378df1502a3
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-06-06 19:19:29 +0000

    Mumuki Export on 2019-06-06 19:19:29 +0000

--- 00008_Indo e vindo/test.php
+++ 00008_Indo e vindo/test.php
@@ -49,35 +49,3 @@ public function testAgregarUsuario2(): void {
   $this->assertTrue($usuariosJSON === $resultado, "O resultado obtido não é o esperado");
 }
 
-public function testAgregarUsuario3(): void {
-  $usuarios = [
-    0 => [
-      "nome" => "Thomaz",
-      "sobrenome" => "Staziak"
-    ],
-    1 => [
-      "nome" => "Victor",
-      "sobrenome" => "Torres"
-    ],
-    2 => [
-      "nome" => "Guilherme",
-      "sobrenome" => "Sartori"
-    ]
-  ];
-  
-  $usuariosJSON = json_encode($usuarios);
-  
-  $novoUsuario = [
-    "nome" => "Chico",
-    "sobrenome" => "Xavier"
-  ];
-  
-  $resultado = agregarUsuario($usuariosJSON, $novoUsuario);
-  
-  $this->assertTrue(is_string($resultado), "O resultado da função deveria ser uma string");
-  
-  $usuarios[] = $novoUsuario;
-  $usuariosJSON = json_encode($usuarios);
-  
-  $this->assertTrue($usuariosJSON === $resultado, "O resultado obtido não é o esperado");
-}
