48fdb83b46ae024fdf096537ca0a4c879cb1b7ff
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-06-06 17:45:13 +0000

    Mumuki Export on 2019-06-06 17:45:13 +0000

--- 00002_Validando extensões/test.php
+++ 00002_Validando extensões/test.php
@@ -15,7 +15,6 @@ public function testJPG(): void {
   $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.jpg'");
 }
 
-
 public function testPNG(): void {
   $_FILES = [
     "avatar" => [
@@ -33,8 +32,6 @@ public function testPNG(): void {
   $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.png'");
 }
 
-
-
 public function testJPEG(): void {
   $_FILES = [
     "avatar" => [
@@ -50,42 +47,6 @@ public function testJPEG(): void {
   $this->assertTrue(is_bool($resultado), "A função não está retornando um valor booleano");
   
   $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.jpeg'");
-}
-
-
-
-public function testTXT(): void {
-  $_FILES = [
-    "avatar" => [
-      "name" => "notas.txt",
-      "type" => "text/plain",
-      "tmp_name" => "/tmp/img/php6hst32",
-      "error" => 0,
-      "size" => 1024
-    ]
-  ];
-  $resultado = $this->validarExtensao();
-  
-  $this->assertTrue(is_bool($resultado), "A função não está retornando um valor booleano");
-  
-  $this->assertFalse($resultado, "A função está falhando para o arquivo 'notas.txt'");
-}
-
-public function testHTML(): void {
-  $_FILES = [
-    "avatar" => [
-      "name" => "home.html",
-      "type" => "text/html",
-      "tmp_name" => "/tmp/img/php6hst32",
-      "error" => 0,
-      "size" => 1024
-    ]
-  ];
-  $resultado = $this->validarExtensao();
-  
-  $this->assertTrue(is_bool($resultado), "A função não está retornando um valor booleano");
-  
-  $this->assertFalse($resultado, "A função está falhando para o arquivo 'home.html'");
 }
 
 public /*...content...*/
