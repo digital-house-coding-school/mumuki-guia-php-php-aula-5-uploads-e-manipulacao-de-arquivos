c53722430eb33c6f7dee1cfc27d336db85dc4924
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-06-06 22:11:05 +0000

    Mumuki Export on 2019-06-06 22:11:05 +0000

--- 00002_Validando extensões/test.php
+++ 00002_Validando extensões/test.php
@@ -16,38 +16,3 @@ public function testJPG(): void {
   $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.jpg'");
 }
 
-public function testPNG(): void {
- /*...content...*/
-  $_FILES = [
-    "avatar" => [
-      "name" => "avatar.png",
-      "type" => "image/png",
-      "tmp_name" => "/tmp/img/php6hst32",
-      "error" => 0,
-      "size" => 1024
-    ]
-  ];
-  $resultado = $this->validarExtensao();
-  
-  $this->assertTrue(is_bool($resultado), "A função não está retornando um valor booleano");
-  
-  $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.png'");
-}
-
-public function testJPEG(): void {
- /*...content...*/
-  $_FILES = [
-    "avatar" => [
-      "name" => "avatar.jpeg",
-      "type" => "image/jpeg",
-      "tmp_name" => "/tmp/img/php6hst32",
-      "error" => 0,
-      "size" => 1024
-    ]
-  ];
-  $resultado = $this->validarExtensao();
-  
-  $this->assertTrue(is_bool($resultado), "A função não está retornando um valor booleano");
-  
-  $this->assertTrue($resultado, "A função está falhando para o arquivo 'avatar.jpeg'");
-}
