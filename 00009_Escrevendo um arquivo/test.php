af7770cd19c9f66f1b446485ed731d416e71327e
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-06-06 17:27:50 +0000

    Mumuki Export on 2019-06-06 17:27:50 +0000

--- 00009_Escrevendo um arquivo/test.php
+++ 00009_Escrevendo um arquivo/test.php
@@ -1,9 +1,9 @@
-public function testArchivo(): void {
+public function testArquivo(): void {
   escreverUmArquivo();
   $this->assertTrue(file_exists("prova.txt"), "O arquivo 'prova.txt' parece não existir");
   
   $text = file_get_contents("prova.txt");
   $text = strtolower($text);
   
-  $this->assertTrue($text === "Meu primeiro arquivo!", "O conteúdo deste arquivo é '$text' e deveria ser 'Meu primeiro arquivo!'");
+  $this->assertTrue($text === "meu primeiro arquivo!", "O conteúdo deste arquivo é '$text' e deveria ser 'Meu primeiro arquivo!'");
 }
